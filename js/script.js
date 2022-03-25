296bd5537a44d7e6fb8e3aa9fbcc5a6a825d62de
Ashley Richard <ashleymrichard@gmail.com>
2022-03-24 20:13:01 -0700

    JS Working

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const APIKEY = "8693e4500be3c4d60e640e7ed276e884"
+const $input = $("input")
+const $button = $("button")
+
+
+
+$button.on("click",() =>{
+    let CITYNAME = $input.val()
+    $.ajax({
+        url:`https://api.openweathermap.org/data/2.5/weather?q=${CITYNAME}&appid=${APIKEY}`
+    }).then((data) => {
+        console.log(data);
+        $('#name').text(`Weather For: ${data.name}`);
+        $('#temp').text(`Temperature: ${Math.round(((data.main.temp-273.15)*1.8)+32)}\xB0`);
+        $('#feelsLike').text(`Feels Like: ${Math.round(((data.main.feels_like-273.15)*1.8)+32)}\xB0`);
+        $('#weather').text(`Weather: ${data.weather[0].description}`);
+    })
+})
